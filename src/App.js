381c16c6022eaa665b740f98ba9d1fe9182c4a61
branmar97 <brandonmar97@gmail.com>
2021-02-10 18:51:43 -0500

    Add filter handler

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
+  // Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
