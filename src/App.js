505d2dba56a52593b1ff9a63882fceca15471665
branmar97 <brandonmar97@gmail.com>
2021-02-10 16:17:08 -0500

    Add form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+// Importing Components
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todolist</h1>
+      <header>
+        <h1>Todolist</h1>
+      </header>
+      <Form />
     </div>
   );
 }
